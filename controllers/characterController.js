1499163c6cf2aac0ca3ce974814b9069cd6e0d42
iachiru <108947252+iachiru@users.noreply.github.com>
2023-02-17 15:14:27 +0000

    up until allegiances

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -21,7 +21,99 @@ const getCharacters = async (req, res) => {
 
 // GET character by name :name
 
+const characterByName = async (req, res) => {
+  try {
+    const { name } = req.params;
+    const response = await axios.get(`${baseURL}/characters/?name=${name}`);
+    const character = response.data;
+    res.json(character);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const characterTitle = async (req, res) => {
+  try {
+    const { name } = req.params;
+    const response = await axios.get(`${baseURL}/characters/?name=${name}`);
+    const character = response.data;
+    const titles = character[0].titles;
+
+    res.json(titles);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const characterByPage = async (req, res) => {
+  try {
+    const { page, pageSize } = req.params;
+    const response = await axios.get(
+      `${baseURL}/characters?page=${page}&pageSize=${pageSize}`
+    );
+    const character = response.data;
+    res.json(character);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const bookByCharacter = async (req, res) => {
+  try {
+    const { name } = req.params;
+    const response = await axios.get(`${baseURL}/characters/?name=${name}`);
+    const character = response.data;
+    const books = character[0].books;
+
+    res.json(books);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const allegianceByCharacter = async (req, res) => {
+  try {
+    const { name } = req.params;
+    const response = await axios.get(`${baseURL}/characters/?name=${name}`);
+    const character = response.data;
+    const allegiances = character[0].allegiances;
+
+    res.json(allegiances);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// get all lords
+
+const getLords = async (req, res) => {
+  try {
+    const response = await axios.get(
+      `${baseURL}/characters?page=5&pageSize=50`
+    );
+    const characters = response.data; // this is an object
+
+    const titles = characters
+      .map((character) => character.titles)
+      .filter((title) => {
+        if (title != "Lord") {
+          title.splice(1);
+        }
+      });
+    res.json(titles);
+  } catch (error) {
+    console.log(error);
+  }
+};
 //how do we send a 500 error with a "Server Error" message?
 
 //export controller funcs
-module.exports = { getCharacters };
+module.exports = {
+  getCharacters,
+  characterByName,
+  characterTitle,
+  characterByPage,
+  bookByCharacter,
+  allegianceByCharacter,
+  getLords,
+};
--- routes/characterRoutes.js
+++ routes/characterRoutes.js
@@ -5,11 +5,27 @@ const router = express.Router();
 //Import characterController
 const {
   getCharacters,
-  getByName,
+  characterTitle,
+  characterByName,
+  characterByPage,
+  bookByCharacter,
+  allegianceByCharacter,
+  getLords,
 } = require("../controllers/characterController");
 //GET all characters from the API
 router.get("/characters", getCharacters);
 //router.get("/characters/name", getByName);
+router.get("/character/name/:name", characterByName);
+// get character titles
+router.get("/character/titles/:name", characterTitle);
+// get chatacter by page
+router.get("/character/page/:page&:pageSize", characterByPage);
+// get books by character
+router.get("/character/books/:name", bookByCharacter);
+// get allegiance by character
+router.get("/character/allegiances/:name", allegianceByCharacter);
+// get all lords
+router.get("/lords", getLords);
 //Export router
 
 module.exports = router;
